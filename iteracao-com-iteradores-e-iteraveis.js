6af9eadd666bcfcba13f1f64444821d7b5cdbf2a
Andressa Miki <andressamiki@gmail.com>
2021-01-24 23:48:43 -0300

    Estudo sobre iteradores

---
+++ iteracao-com-iteradores-e-iteraveis.js
@@ -0,0 +1,38 @@
+/*iteraveis
+- arrays
+- strings
+- maps
+- sets*/
+
+
+// Iteração com o laço for...of
+let numeros = [1,3,4];
+for (var iterator of numeros) {
+    console.log(iterator)
+}
+
+// Diferenças entre  o for...of e for...in
+let perfilDoFacebook = {
+    nome: 'Andressa',
+    idade: 25
+}
+
+for (var key in perfilDoFacebook) {
+    let info = perfilDoFacebook[key];
+    console.log(info);
+}
+
+// Break e continue 
+for (var iterator of numeros) {
+    if(iterator > 2){
+        break;
+    }
+}
+
+for (var iterator of numeros) {
+    if(iterator === 3){
+        continue;
+    }
+}
+
+//Estruturas de map e weakmap
