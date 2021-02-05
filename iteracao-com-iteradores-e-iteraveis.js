b82f8ed8fa9d9e3ac8f229fa04dd66424bb6b3f0
Andressa Miki <andressamiki@gmail.com>
2021-02-04 22:30:53 -0300

    Estudando set e weakmap

--- iteracao-com-iteradores-e-iteraveis.js
+++ iteracao-com-iteradores-e-iteraveis.js
@@ -36,5 +36,32 @@ for (var iterator of numeros) {
 }
 
 //Estruturas de map e weakmap
+let mapa = new Map();
+mapa.set('um', 1);
+mapa.set('dois', 2);
+mapa.set('tres', 3);
+
+for (var chave of mapa.keys()) {
+    console.log(chave);
+}
+
+for (var valor of mapa.values()) {
+    console.log(valor)
+}
+
+for (var entrada of mapa.entries()) {
+    console.log(entrada);
+}
+
+//Set
+
+let set = new Set();
+set.add(1);
+set.add(2);
+set.add(1);
+
+for (const valor of set) {
+    console.log(valor) //1,2
+}
 
 
